dc530eee839091a9a5dd9b55fe69c669a8aa6642
shyam2tayal <shyam2tayal@gmail.com>
2015-09-10 14:48:08 +0530

    new 1

--- shyam/js/custom.js
+++ shyam/js/custom.js
@@ -1,29 +1,98 @@
-function debounce(a,b,c){var d;return function(){var e=this,f=arguments;clearTimeout(d),d=setTimeout(function(){d=null,c||a.apply(e,f)},b),c&&!d&&a.apply(e,f)}}
+/*global jQuery:false */
+(function ($) {
 
-skrollrCheck = debounce(function() {
-  var winWidth        = window.innerWidth;
-  if(winWidth >= 600) {
-    // Init Skrollr
-    skrollr.init({
-        forceHeight: false,//disable setting height on body
-        mobileDeceleration:0.05,
-        render: function(data) {
-            //Debugging - Log the current scroll position.
-            //console.log('data.curTop: ' + data.curTop);
-        }
+
+	$(window).load(function(){
+      $("#navigation").sticky({ topSpacing: 0 });
     });
-    skrollr.get().refresh();
-  } else {
-    // Destroy skrollr for screens less than 600px
-    skrollr.init().destroy();
-  }
-}, 250);
+	
 
-//Initialize skrollr, but only if it exists
-if(typeof skrollr !== typeof undefined){
-  // INITIALIZE
-  window.onload = skrollrCheck();
-  window.addEventListener('resize', skrollrCheck);
-} else {
-  console.log('skrollr is missing.');
-}
+	wow = new WOW(
+      {
+        animateClass: 'animated',
+        offset:       0
+      }
+    );
+    wow.init();
+
+	$('ul.nav li.dropdown').hover(function() {
+	  $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(500);
+	}, function() {
+	  $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
+	});	
+
+	
+	//jQuery to collapse the navbar on scroll
+	$(window).scroll(function() {
+		if ($(".navbar").offset().top > 50) {
+			$(".navbar-fixed-top").addClass("top-nav-collapse");
+		} else {
+			$(".navbar-fixed-top").removeClass("top-nav-collapse");
+		}
+	});
+	
+	//jQuery for page scrolling feature - requires jQuery Easing plugin
+	$(function() {
+		$('.navbar-nav li a').bind('click', function(event) {
+			var $anchor = $(this);
+			var nav = $($anchor.attr('href'));
+			if (nav.length) {
+			$('html, body').stop().animate({				
+				scrollTop: $($anchor.attr('href')).offset().top				
+			}, 1500, 'easeInOutExpo');
+			
+			event.preventDefault();
+			}
+		});
+		$('a.totop,a#btn-scroll,a.btn-slide,.carousel-inner .item a.btn').bind('click', function(event) {
+			var $anchor = $(this);
+			$('html, body').stop().animate({
+				scrollTop: $($anchor.attr('href')).offset().top
+			}, 1500, 'easeInOutExpo');
+			event.preventDefault();
+		});
+	});
+
+	
+	$('.testimonialslide').flexslider({
+	animation: "slide",
+	slideshow: false,
+	directionNav:false,
+	controlNav: true
+	});
+	
+	//owl carousel
+	$('#owl-works').owlCarousel({
+            items : 4,
+            itemsDesktop : [1199,5],
+            itemsDesktopSmall : [980,5],
+            itemsTablet: [768,5],
+            itemsTabletSmall: [550,2],
+            itemsMobile : [480,2],
+        });
+	
+	//nivo lightbox
+	$('.owl-carousel .item a').nivoLightbox({
+		effect: 'fadeScale',                             // The effect to use when showing the lightbox
+		theme: 'default',                           // The lightbox theme to use
+		keyboardNav: true,                          // Enable/Disable keyboard navigation (left/right/escape)
+		clickOverlayToClose: true,                  // If false clicking the "close" button will be the only way to close the lightbox
+		onInit: function(){},                       // Callback when lightbox has loaded
+		beforeShowLightbox: function(){},           // Callback before the lightbox is shown
+		afterShowLightbox: function(lightbox){},    // Callback after the lightbox is shown
+		beforeHideLightbox: function(){},           // Callback before the lightbox is hidden
+		afterHideLightbox: function(){},            // Callback after the lightbox is hidden
+		onPrev: function(element){},                // Callback when the lightbox gallery goes to previous item
+		onNext: function(element){},                // Callback when the lightbox gallery goes to next item
+		errorMessage: 'The requested content cannot be loaded. Please try again later.' // Error message when content can't be loaded
+	});
+	
+
+	jQuery('.appear').appear();
+	jQuery(".appear").on("appear", function(data) {
+			var id = $(this).attr("id");
+			jQuery('.nav li').removeClass('active');
+			jQuery(".nav a[href='#" + id + "']").parent().addClass("active");					
+		});
+	
+})(jQuery);
