c7a78c499711061d4c4bcc942f7e7236d1047738
shyam2tayal <shyam2tayal@gmail.com>
2016-08-31 02:35:52 +0530

    hope it works

--- ui-router/app.js
+++ ui-router/app.js
@@ -7,41 +7,11 @@ App.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $ur
 				$urlRouterProvider.otherwise("/business")
 				
 				$stateProvider
-						.state('business', {
-							url: "/business",
-							templateUrl: "business.html"
-						})
-
+						
 						.state('display', {
 							url: "/display",
 							templateUrl: "display.html"
 						})
 
-						.state('business.products', {
-							url: "/products",
-							templateUrl: "products.html",
-							controller: function($scope){
-								$scope.products = ["Computer", "Printers", "Phones", "Bags"];
-							}
-						})
-						.state('business.services', {
-							url: "/services",
-							templateUrl: "services.html",
-							controller: function($scope){
-								$scope.services = ["Selling", "Support", "Delivery", "Reparation"];
-							}
-						})
-
-						.state('portfolio', {
-							url: "/portfolio",
-							views: {
-								"" 	:    { templateUrl: "portfolio.html" },
-								"view1@portfolio": { template: "Write whatever you want, it's your virtual company." },
-								"view2@portfolio": { templateUrl: "clients.html" ,
-									controller: function($scope){
-											$scope.clients = ["HP", "IBM", "MicroSoft"];
-									}
-								}
-							}
-						})
+					
 			}]);
